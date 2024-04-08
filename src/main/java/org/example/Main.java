b38eec5d6d5f6e2dfc8cb8cd16d3279456b53d2b
EtanaAlemu <etanaalemunew@gmail.com>
2024-04-08 19:48:53 -0400

    parse success trx

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.example.Utils.*;
 
@@ -67,18 +69,18 @@ public class Main {
 //        System.out.println("Value: " + value);
     }
 
-    public static List<Transaction> parseEJFile(String filename) {
-        List<Transaction> transactions = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            String line;
-            Transaction currentTransaction = null;
-            while ((line = br.readLine()) != null) {
-                if (line.startsWith("============TRANSACTION START=============")) {
-                    if (currentTransaction != null) {
-                        transactions.add(currentTransaction);
-                    }
-                    currentTransaction = new Transaction();
-                } else {
+        public static List<Transaction> parseEJFile(String filename) {
+            List<Transaction> transactions = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+                String line;
+                Transaction currentTransaction = null;
+                while ((line = br.readLine()) != null) {
+                    if (line.startsWith("============TRANSACTION START=============")) {
+                        if (currentTransaction != null) {
+                            transactions.add(currentTransaction);
+                        }
+                        currentTransaction = new Transaction();
+                    } else {
                     if (line.contains("CARD:")) {
                         String[] cardEvent = splitCardLine(line);
                         Event event = new Event(cardEvent[0], cardEvent[1], cardEvent[2]);
@@ -95,6 +97,78 @@ public class Main {
                         String[] aidEvent = splitAIDLine(line);
                         Event event = new Event(aidEvent[0], aidEvent[1], aidEvent[2]);
                         currentTransaction.addEvent(event);
+                    } else if (line.contains("PIN")) {
+                        String[] pinEvent = splitPINLine(line);
+                        Event event = new Event(pinEvent[0], pinEvent[1], pinEvent[2]);
+                        currentTransaction.addEvent(event);
+                    } else if (line.contains("AMOUNT/INFORMATION")) {
+                        String[] amountInformationFDKEvent = splitAmountInformationFDKLine(line);
+                        Event event = new Event(amountInformationFDKEvent[0], amountInformationFDKEvent[1], amountInformationFDKEvent[2]);
+                        currentTransaction.addEvent(event);
+                    } else if (line.contains("1st GenAC:P1")) {
+                        String[] genACEvent = splitGenACLine(line);
+                        Event event = new Event(genACEvent[0], genACEvent[1], genACEvent[2]);
+                        currentTransaction.addEvent(event);
+                    } else if (line.contains("TNX REQUEST")) {
+                        String[] tnxRequestEvent = splitTNXRequestLine(line);
+                        Event event = new Event(tnxRequestEvent[0], tnxRequestEvent[1], tnxRequestEvent[2]);
+                        currentTransaction.addEvent(event);
+                    } else if (line.contains("TNX REPLY")) {
+                        String[] tnxReplyEvent = splitTNXReplyLine(line);
+                        Event event = new Event(tnxReplyEvent[0], tnxReplyEvent[1], tnxReplyEvent[2]);
+                        currentTransaction.addEvent(event);
+                    } else if (line.contains("2nd GenAC:P1")) {
+                        String[] genAC2Event = splitGenAC2Line(line);
+                        Event event = new Event(genAC2Event[0], genAC2Event[1], genAC2Event[2]);
+                        currentTransaction.addEvent(event);
+                    } else if (line.contains("CARD TAKEN BY CUSTOMER")) {
+                        String[] cardTakenEvent = splitCardTakenLine(line);
+                        Event event = new Event(cardTakenEvent[0], cardTakenEvent[1], cardTakenEvent[2]);
+                        currentTransaction.addEvent(event);
+                    } else if (line.contains("NOTES PRESENT AT CASH SLOT")) {
+                        String[] notesPresentEvent = splitNotesPresentLine(line);
+                        Event event = new Event(notesPresentEvent[0], notesPresentEvent[1], notesPresentEvent[2]);
+                        currentTransaction.addEvent(event);
+                    } else if (line.contains("CASH SHUTTER OPENED")) {
+                        String[] cashShutterOpenedEvent = splitCashShutterOpenedLine(line);
+                        Event event = new Event(cashShutterOpenedEvent[0], cashShutterOpenedEvent[1], cashShutterOpenedEvent[2]);
+                        currentTransaction.addEvent(event);
+                    } else if (line.contains("NOTES TAKEN BY CUSTOMER")) {
+                        String[] notesTakenEvent = splitNotesTakenLine(line);
+                        Event event = new Event(notesTakenEvent[0], notesTakenEvent[1], notesTakenEvent[2]);
+                        currentTransaction.addEvent(event);
+                    } else if (line.contains("CASH SHUTTER CLOSED")) {
+                        String[] cashShutterClosedEvent = splitCashShutterClosedLine(line);
+                        Event event = new Event(cashShutterClosedEvent[0], cashShutterClosedEvent[1], cashShutterClosedEvent[2]);
+                        currentTransaction.addEvent(event);
+                    } else if (line.matches("^\\d{3}\\s\\d{4}\\s\\d{6}\\s*$")) {
+                        currentTransaction.setBranchCode(line.substring(0, 3).trim());
+                        currentTransaction.setAuthorizationReference(line.substring(4, 8).trim());
+                        currentTransaction.setUnknownData(line.substring(9).trim());
+                    } else if (line.matches("^\\d{7}\\*{8}\\d{4}\\s*$")) {
+                        currentTransaction.setPAN(line.trim());
+                    } else if (line.matches("^\\s*\\d+\\.\\d+\\s+\\d+\\s*$")) {
+                        String[] parts = line.split("\\s+");
+                        currentTransaction.setWithdrawalAmount(parts[1]);
+                        currentTransaction.setCurrencyCode(parts[2]);
+                    } else if (line.startsWith("SURCHARGE:")) {
+                        currentTransaction.setSurcharge(line.substring(10).trim());
+                    } else if (line.matches("\\d+")) {
+                        currentTransaction.setChargedFee(line);
+                    } else if (line.startsWith("DISPENSE")) {
+                        currentTransaction.setDispenseEvent(line);
+                    } else if (line.matches("^\\s*([A-Z]+)\\s*(\\d+)\\s*(-?\\d+)\\s*$")) {
+                        Pattern pattern = Pattern.compile("([A-Z]+)\\s*(\\d+)\\s*(-?\\d+)");
+                        Matcher matcher = pattern.matcher(line);
+
+                        if (matcher.find()) {
+                            String currency = matcher.group(1);
+                            String amount = matcher.group(2);
+                            String unknownAmount = matcher.group(3);
+
+
+                            currentTransaction.setDispensedAmount(amount);
+                        }
                     } else {
                         // Ignore unknown lines
                     }
--- src/main/java/org/example/Utils.java
+++ src/main/java/org/example/Utils.java
@@ -108,4 +108,271 @@ public class Utils {
         }
         return new String[]{time, key, value};
     }
+
+    public static String[] splitPINLine(String line) {
+        // Find the index of the first occurrence of ' ' (whitespace) to split time and key-value pair
+        int spaceIndex = line.indexOf(' ');
+        // Extract time
+        String time = line.substring(0, spaceIndex).trim();
+        // Extract key-value pair
+        String keyValue = line.substring(spaceIndex).trim();
+        // Find the index of the first occurrence of ' ' (whitespace) within the key-value pair to split key and value
+        int secondSpaceIndex = keyValue.indexOf(' ');
+        // Extract key
+        String key;
+        // Extract value
+        String value;
+        if (secondSpaceIndex != -1) {
+            // If the line contains a second space, split it into key and value
+            key = keyValue.substring(0, secondSpaceIndex).trim();
+            value = keyValue.substring(secondSpaceIndex + 1).trim();
+        } else {
+            // If there's no second space, consider the entire line as key and set an empty string as value
+            key = keyValue.trim();
+            value = ""; // No value present
+        }
+        return new String[]{time, key, value};
+    }
+
+    public static String[] splitAmountInformationFDKLine(String line) {
+        // Find the index of the first occurrence of ' ' (whitespace) to split time and key-value pair
+        int spaceIndex = line.indexOf(' ');
+        // Extract time
+        String time = line.substring(0, spaceIndex).trim();
+        // Extract key-value pair
+        String keyValue = line.substring(spaceIndex).trim();
+        // Find the index of the first occurrence of ' ' (whitespace) within the key-value pair to split key and value
+        int secondSpaceIndex = keyValue.indexOf(' ');
+        // Extract key
+        String key;
+        // Extract value
+        String value;
+        if (secondSpaceIndex != -1) {
+            // If the line contains a second space, split it into key and value
+            key = keyValue.substring(0, secondSpaceIndex).trim();
+            value = keyValue.substring(secondSpaceIndex + 1).trim();
+        } else {
+            // If there's no second space, consider the entire line as key and set an empty string as value
+            key = keyValue.trim();
+            value = ""; // No value present
+        }
+        return new String[]{time, key, value};
+    }
+    public static String[] splitGenACLine(String line) {
+        // Find the index of the first occurrence of ' ' (whitespace) to split time and key-value pair
+        int spaceIndex = line.indexOf(' ');
+        // Extract time
+        String time = line.substring(0, spaceIndex).trim();
+        // Extract key-value pair
+        String keyValue = line.substring(spaceIndex).trim();
+        // Find the index of the first occurrence of ' ' (whitespace) within the key-value pair to split key and value
+        int secondSpaceIndex = keyValue.indexOf(' ');
+        // Extract key
+        String key;
+        // Extract value
+        String value;
+        if (secondSpaceIndex != -1) {
+            // If the line contains a second space, split it into key and value
+            key = keyValue.substring(0, secondSpaceIndex).trim();
+            value = keyValue.substring(secondSpaceIndex + 1).trim();
+        } else {
+            // If there's no second space, consider the entire line as key and set an empty string as value
+            key = keyValue.trim();
+            value = ""; // No value present
+        }
+        return new String[]{time, key, value};
+    }
+    public static String[] splitTNXRequestLine(String line) {
+        // Find the index of the first occurrence of ' ' (whitespace) to split time and key-value pair
+        int spaceIndex = line.indexOf(' ');
+        // Extract time
+        String time = line.substring(0, spaceIndex).trim();
+        // Extract key-value pair
+        String keyValue = line.substring(spaceIndex).trim();
+        // Find the index of the first occurrence of ',' within the key-value pair to split key and value
+        int commaIndex = keyValue.indexOf(',');
+        // Extract key
+        String key;
+        // Extract value
+        String value;
+        if (commaIndex != -1) {
+            // If the line contains ',', split it into key and value
+            key = keyValue.substring(0, commaIndex).trim();
+            value = keyValue.substring(commaIndex + 1).trim();
+        } else {
+            // If there's no ',', consider the entire line as key and set an empty string as value
+            key = keyValue.trim();
+            value = ""; // No value present
+        }
+        return new String[]{time, key, value};
+    }
+    public static String[] splitTNXReplyLine(String line) {
+        // Find the index of the first occurrence of ' ' (whitespace) to split time and key-value pair
+        int spaceIndex = line.indexOf(' ');
+        // Extract time
+        String time = line.substring(0, spaceIndex).trim();
+        // Extract key-value pair
+        String keyValue = line.substring(spaceIndex).trim();
+        // Find the index of the first occurrence of ',' within the key-value pair to split key and value
+        int commaIndex = keyValue.indexOf(',');
+        // Extract key
+        String key;
+        // Extract value
+        String value;
+        if (commaIndex != -1) {
+            // If the line contains ',', split it into key and value
+            key = keyValue.substring(0, commaIndex).trim();
+            value = keyValue.substring(commaIndex + 1).trim();
+        } else {
+            // If there's no ',', consider the entire line as key and set an empty string as value
+            key = keyValue.trim();
+            value = ""; // No value present
+        }
+        return new String[]{time, key, value};
+    }
+    public static String[] splitGenAC2Line(String line) {
+        // Find the index of the first occurrence of ' ' (whitespace) to split time and key-value pair
+        int spaceIndex = line.indexOf(' ');
+        // Extract time
+        String time = line.substring(0, spaceIndex).trim();
+        // Extract key-value pair
+        String keyValue = line.substring(spaceIndex).trim();
+        // Find the index of the first occurrence of ' ' (whitespace) within the key-value pair to split key and value
+        int secondSpaceIndex = keyValue.indexOf(' ');
+        // Extract key
+        String key;
+        // Extract value
+        String value;
+        if (secondSpaceIndex != -1) {
+            // If the line contains a second space, split it into key and value
+            key = keyValue.substring(0, secondSpaceIndex).trim();
+            value = keyValue.substring(secondSpaceIndex + 1).trim();
+        } else {
+            // If there's no second space, consider the entire line as key and set an empty string as value
+            key = keyValue.trim();
+            value = ""; // No value present
+        }
+        return new String[]{time, key, value};
+    }
+    public static String[] splitCardTakenLine(String line) {
+        // Find the index of the first occurrence of ' ' (whitespace) to split time and key-value pair
+        int spaceIndex = line.indexOf(' ');
+        // Extract time
+        String time = line.substring(0, spaceIndex).trim();
+        // Extract key-value pair
+        String keyValue = line.substring(spaceIndex).trim();
+        // Find the index of the first occurrence of ' ' (whitespace) within the key-value pair to split key and value
+        int secondSpaceIndex = keyValue.indexOf(' ');
+        // Extract key
+        String key;
+        // Extract value
+        String value;
+        if (secondSpaceIndex != -1) {
+            // If the line contains a second space, split it into key and value
+            key = keyValue.substring(0, secondSpaceIndex).trim();
+            value = keyValue.substring(secondSpaceIndex + 1).trim();
+        } else {
+            // If there's no second space, consider the entire line as key and set an empty string as value
+            key = keyValue.trim();
+            value = ""; // No value present
+        }
+        return new String[]{time, key, value};
+    }
+    public static String[] splitNotesPresentLine(String line) {
+        // Find the index of the first occurrence of ' ' (whitespace) to split time and key-value pair
+        int spaceIndex = line.indexOf(' ');
+        // Extract time
+        String time = line.substring(0, spaceIndex).trim();
+        // Extract key-value pair
+        String keyValue = line.substring(spaceIndex).trim();
+        // Find the index of the first occurrence of ' ' (whitespace) within the key-value pair to split key and value
+        int secondSpaceIndex = keyValue.indexOf(' ');
+        // Extract key
+        String key;
+        // Extract value
+        String value;
+        if (secondSpaceIndex != -1) {
+            // If the line contains a second space, split it into key and value
+            key = keyValue.substring(0, secondSpaceIndex).trim();
+            value = keyValue.substring(secondSpaceIndex + 1).trim();
+        } else {
+            // If there's no second space, consider the entire line as key and set an empty string as value
+            key = keyValue.trim();
+            value = ""; // No value present
+        }
+        return new String[]{time, key, value};
+    }
+    public static String[] splitCashShutterOpenedLine(String line) {
+        // Find the index of the first occurrence of ' ' (whitespace) to split time and key-value pair
+        int spaceIndex = line.indexOf(' ');
+        // Extract time
+        String time = line.substring(0, spaceIndex).trim();
+        // Extract key-value pair
+        String keyValue = line.substring(spaceIndex).trim();
+        // Find the index of the first occurrence of ' ' (whitespace) within the key-value pair to split key and value
+        int secondSpaceIndex = keyValue.indexOf(' ');
+        // Extract key
+        String key;
+        // Extract value
+        String value;
+        if (secondSpaceIndex != -1) {
+            // If the line contains a second space, split it into key and value
+            key = keyValue.substring(0, secondSpaceIndex).trim();
+            value = keyValue.substring(secondSpaceIndex + 1).trim();
+        } else {
+            // If there's no second space, consider the entire line as key and set an empty string as value
+            key = keyValue.trim();
+            value = ""; // No value present
+        }
+        return new String[]{time, key, value};
+    }
+    public static String[] splitNotesTakenLine(String line) {
+        // Find the index of the first occurrence of ' ' (whitespace) to split time and key-value pair
+        int spaceIndex = line.indexOf(' ');
+        // Extract time
+        String time = line.substring(0, spaceIndex).trim();
+        // Extract key-value pair
+        String keyValue = line.substring(spaceIndex).trim();
+        // Find the index of the first occurrence of ' ' (whitespace) within the key-value pair to split key and value
+        int secondSpaceIndex = keyValue.indexOf(' ');
+        // Extract key
+        String key;
+        // Extract value
+        String value;
+        if (secondSpaceIndex != -1) {
+            // If the line contains a second space, split it into key and value
+            key = keyValue.substring(0, secondSpaceIndex).trim();
+            value = keyValue.substring(secondSpaceIndex + 1).trim();
+        } else {
+            // If there's no second space, consider the entire line as key and set an empty string as value
+            key = keyValue.trim();
+            value = ""; // No value present
+        }
+        return new String[]{time, key, value};
+    }
+    public static String[] splitCashShutterClosedLine(String line) {
+        // Find the index of the first occurrence of ' ' (whitespace) to split time and key-value pair
+        int spaceIndex = line.indexOf(' ');
+        // Extract time
+        String time = line.substring(0, spaceIndex).trim();
+        // Extract key-value pair
+        String keyValue = line.substring(spaceIndex).trim();
+        // Find the index of the first occurrence of ' ' (whitespace) within the key-value pair to split key and value
+        int secondSpaceIndex = keyValue.indexOf(' ');
+        // Extract key
+        String key;
+        // Extract value
+        String value;
+        if (secondSpaceIndex != -1) {
+            // If the line contains a second space, split it into key and value
+            key = keyValue.substring(0, secondSpaceIndex).trim();
+            value = keyValue.substring(secondSpaceIndex + 1).trim();
+        } else {
+            // If there's no second space, consider the entire line as key and set an empty string as value
+            key = keyValue.trim();
+            value = ""; // No value present
+        }
+        return new String[]{time, key, value};
+    }
+
 }
